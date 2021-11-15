39c367f01cbab946c5b83916986000baab19d455
Giacomo Bergami <bergamigiacomo@gmail.com>
2021-11-15 13:17:49 +0000

    Interval tree: adding some features that are coming from the double/string implementations, and that are general enough to be put at the parent level

--- include/yaucl/structures/query_interval_set/structures/segment_partition_tree.h
+++ include/yaucl/structures/query_interval_set/structures/segment_partition_tree.h
@@ -27,12 +27,15 @@
 #define CUCCIOLO_SEGMENT_PARTITION_TREE_H
 
 #include <yaucl/structures/query_interval_set/structures/node_recur.h>
+#include <yaucl/structures/query_interval_set/algorithms/minimize_tree.h>
 
 template <typename T, typename PrevNext> struct segment_partition_tree {
     PrevNext     indexer;
     struct node_recur<T> element;
+    T min;
+    T max;
 
-    segment_partition_tree(T min, T max) : element(min, max) {}
+    segment_partition_tree(T min, T max) : element(min, max), min{min}, max{max} {}
     segment_partition_tree() : element{} {}
     segment_partition_tree(const segment_partition_tree& ) = default;
     segment_partition_tree(segment_partition_tree&&      ) = default;
@@ -50,6 +53,30 @@ template <typename T, typename PrevNext> struct segment_partition_tree {
         std::unordered_set<std::pair<T,T>> S;
         element.collect_intervals2(S, indexer);
         return S;
+    }
+
+    std::vector<std::pair<T,T>> bulk_load;
+
+    void load_phase_insert_data(const T& lowerBound, const T& upperBound) {
+        bulk_load.emplace_back(lowerBound, upperBound);
+    }
+
+    void index_phase() {
+        bulk_load.emplace_back(min, max);
+        std::sort(bulk_load.begin(), bulk_load.end(), interval_comparator_inverse<std::string, StringPrevNext>());
+        // Inserting one interval at a time
+        bulk_load.erase( unique( bulk_insertion.begin(), bulk_insertion.end() ), bulk_insertion.end() );
+        for (const auto& i : bulk_load) {
+            /*std::cout << i << " = " <<*/ insert_interval(this->indexer, this->element, i.first, i.second) /*<< std::endl*/;
+            ///std::cout << tree << std::endl;
+            ///std::cout << std::endl;
+        }
+        //std::cout << *this << std::endl;
+        minimize_tree<T, PrevNext>(indexer, element);
+        /*for (const auto& i : bulk_insertion) {
+            std::cout << i << " = " << find_interval(indexer, element, i.first, i.second) << std::endl;
+        }*/
+        //std::cout << *this << std::endl;
     }
 
     friend std::ostream &operator<<(std::ostream &os, const segment_partition_tree &tree) {
