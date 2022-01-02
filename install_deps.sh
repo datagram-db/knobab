941204503b188afe856b40b15f81757eb01eb5e7
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-01-02 01:16:24 +0000

    Update on the dependencies

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 # Installing the required packages
-sudo apt-get install -y uuid-dev maven libboost1.71-all-dev librapidxml-dev
+sudo apt-get install -y uuid-dev maven libboost-python1.74-dev
+pip install git+https://github.com/whitemech/flloat.git
 
 ## Pulling the repositories
 git submodule update --init --recursive
