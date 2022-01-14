375263c109d57c20e19e64d3dddcb9fa0a911d9a
Samuel Appleby <S.Appleby3@newcastle.ac.uk>
2022-01-14 16:19:23 +0000

    Added exists

--- main.cpp
+++ main.cpp
@@ -1,12 +1,41 @@
 #include <iostream>
 #include "yaucl/bpm/structures/log/data_loader.h"
 
-void test_kb() {
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"a", {}}, {"a", {}}, {"b", {}}}};
+
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"a", {}}, {"b", {}}, {"a", {}}, {"c", {}}, {"a", {}}}};
+
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"c", {}}, {"b", {}}, {"c", {}}, {"c", {}}, {"c", {}}}};
+
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"c", {}}, {"c", {}}, {"a", {}}, {"c", {}}, {"c", {}}}};
+//
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"c", {}}, {"c", {}}, {"a", {}}, {"c", {}}, {"c", {}}, {"a", {}}}};
+
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"c", {}}, {"b", {}}, {"c", {}}, {"b", {}}, {"c", {}}}};
+
+//const KnowledgeBase::no_antlr_log LogTrace = {
+//        {{"a", {}}, {"a", {}}, {"b", {}}},
+//{{"a", {}}, {"c", {{"x", 2.0}}}}};
+
+const KnowledgeBase::no_antlr_log LogTrace = {
+        {{"a", {}}, {"a", {}}, {"a", {}}, {"a", {}}, {"b", {}}},
+        {{"b", {}}, {"b", {}}, {"b", {}}, {"b", {}}, {"a", {}}},
+        {{"c", {}}, {"b", {}}, {"c", {}}, {"b", {}}, {"c", {}}}};
+
+KnowledgeBase test_kb(const KnowledgeBase::no_antlr_log& L, const std::string &source, const std::string &name) {
     /// Creating an instance of the knowledge base, that is going to store all the traces in the log!
     KnowledgeBase db;
 
+    db.load_data_without_antlr4(L, source, name);
+
     /// Loading a log file into the database
-    load_into_knowledge_base(HUMAN_READABLE_YAUCL, true, "testing/log.txt", db);
+    /// load_into_knowledge_base(HUMAN_READABLE_YAUCL, true, "testing/log.txt", db);
 
     /// Indexing the data structures
     /// TODO: these indices might be written in secondary memory as well!
@@ -16,9 +45,18 @@ void test_kb() {
     /// This is just an isomorphism proof, that states that I can always reconstruct the original information from the
     /// given representation
     db.reconstruct_trace_with_data(std::cout);
+
+    return db;
 }
 
 int main() {
-    test_kb();
+    KnowledgeBase db = test_kb(LogTrace, "", "");
+
+    // First part
+    uint32_t a, b;
+    auto data = db.resolveCountingData("a", a, b);
+
+    // Second part
+    std::unordered_map<uint32_t, float> found = db.exists(data, a, b, 3);
     return 0;
 }
