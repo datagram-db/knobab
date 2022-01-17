0171d0638d563005afe6e1af3d13a37448135b8b
Giacomo Bergami <bergamigiacomo@gmail.com>
2022-01-17 14:43:53 +0000

    Approximate String Matching was imported, from the secondary memory version of it.

--- include/knobab/AttributeTable.h
+++ include/knobab/AttributeTable.h
@@ -8,6 +8,7 @@
 #include <string>
 #include <knobab/oid.h>
 #include <variant>
+#include <yaucl/bpm/structures/commons/DataPredicate.h>
 class SimplifiedFuzzyStringMatching;
 
 enum AttributeTableType {
@@ -32,18 +33,51 @@ struct AttributeTable {
     SimplifiedFuzzyStringMatching* ptr;
     AttributeTableType type;
 
+
     struct record {
         act_t   act;
         size_t  value;
         size_t  act_table_offset;
 
         record(act_t act, size_t value, size_t actTableOffset);
-        record(const record& ) = default;
-        record(record&& ) = default;
-        record& operator=(const record& ) = default;
-        record& operator=(record&& ) = default;
+        DEFAULT_COPY_ASSGN(record);
     };
     std::vector<record> table;
+
+
+    struct disjunctive_range_query_result {
+        /**
+         * Constructor to be used when an exact solution is always the case
+         */
+        disjunctive_range_query_result(const record* begin, const record* end) : exact_solution(begin, end) {}
+        DEFAULT_CONSTRUCTORS(disjunctive_range_query_result);
+
+        /**
+         * If the query is an approximated query, I cannot efficiently just provide a range of values where the values
+         * are known for sure/certain that are going to be returned.
+         */
+        std::unordered_map<const record*, double> approx_solution;
+
+        /**
+         * If the query requires the exact solution, the range of the records is return, without the need of copying the
+         * data, inefficiencly, with some data duplication issues. This can be still returned if the query if the query
+         * is approximated, and there is still some data that we want to return, and therefore, some exact matches are
+         * found
+         */
+        std::pair<const record*, const record*> exact_solution;
+    };
+    using range_query_result = std::vector<disjunctive_range_query_result>;
+
+    /**
+     * Mapping the act id to the offset of where it appears within the record
+     */
+    std::vector<std::pair<size_t, size_t>> primary_index;
+
+    /**
+     * This data structure maps an offset in the ActTable, identifying a specific event with a specific act/event-label
+     * A within a given trace/run, to an offset within the current table, thus stating that there exists an attribute
+     * A.attr_name in the payload
+     */
     std::unordered_map<size_t, size_t> secondary_index;
 
     AttributeTable() : attr_name(""), ptr{nullptr}, type{BoolAtt} {}
@@ -56,8 +90,20 @@ struct AttributeTable {
     std::ostream& resolve_and_print(std::ostream& os, const AttributeTable::record &x) const;
 
     void record_load(act_t act_id, const union_type& val, trace_t tid, event_t eid);
-    void index(const std::vector<std::vector<size_t>> &trace_id_to_event_id_to_offset, size_t maxOffset);
+    void index(const std::vector<std::vector<size_t>> &trace_id_to_event_id_to_offset);
     union_type resolve(const record& x) const;
+
+    /**
+     *
+     * @param prop              Predicate that we want to test
+     * @param act               Act over which we want to perform the query. If -1, then it means that we want to retrieve them all
+     * @param maximumApprox     If zero, it means that we only want to have exact queries. Otherwise, if a positive value is given,
+     *                          this provides the maximum degree of approximation that is expected
+     * @param c                 For distance functions over non-strings, it provides the distance among all of the elements that have
+     *                          less distance than the given one
+     * @return
+     */
+    range_query_result range_query(const DataPredicate &prop, ssize_t act = -1, double maximumApprox = 0.0, const double c = 2.0) const;
 
 private:
     std::vector<std::map<union_type, std::vector<std::pair<trace_t, event_t>>>> elements;
--- src/knobab/AttributeTable.cpp
+++ src/knobab/AttributeTable.cpp
@@ -6,6 +6,38 @@
 #include "SimplifiedFuzzyStringMatching.h"
 #include <cassert>
 
+static inline union_type cast_unions(AttributeTableType type, const union_minimal &x) {
+    switch (type) {
+        case DoubleAtt:
+            return std::get<double>(x);
+        case LongAtt:
+            return (long long)std::get<double>(x);
+        case StringAtt:
+            return  std::get<std::string>(x);
+        case BoolAtt:
+            return std::abs(std::get<double>(x)) > std::numeric_limits<double>::epsilon();
+            //case SizeTAtt:
+        default:
+            // TODO: hierarchical types!, https://dl.acm.org/doi/10.1145/3410566.3410583
+            return (size_t)std::get<double>(x);
+    }
+}
+
+static inline double distanceFunction(const union_type& lhs, const union_type& rhs, double c = 2.0) {
+    if (lhs.index() != rhs.index())
+        return std::numeric_limits<double>::max(); // If the elements are not of the same type, then they are associated to the maximum distance.
+    if (std::holds_alternative<double>(lhs)) {
+        return 1.0 / ( (std::abs(std::get<double>(lhs)- std::get<double>(rhs)))/c + 1.0);
+    } else if (std::holds_alternative<size_t>(lhs)) {
+        auto l = std::get<size_t>(lhs);
+        auto r = std::get<size_t>(rhs);
+        return 1.0 / ( ((double)(((l > r) ? (l-r) : (r-l))))/c + 1.0);
+    } else if (std::holds_alternative<long long>(lhs)) {
+        return 1.0 / ( (std::abs((double)std::get<long long>(lhs)- (double)std::get<long long>(rhs)))/c + 1.0);
+    }
+
+}
+
 union_type AttributeTable::resolve(const AttributeTable::record &x) const {
     switch (type) {
         case DoubleAtt:
@@ -75,21 +107,31 @@ size_t AttributeTable::storeLoad(const std::variant<double, size_t, long long in
     }
 }
 
-void AttributeTable::index(const std::vector<std::vector<size_t>> &trace_id_to_event_id_to_offset, size_t maxOffset) {
+void AttributeTable::index(const std::vector<std::vector<size_t>> &trace_id_to_event_id_to_offset) {
     for (size_t act_id = 0, N = elements.size(); act_id < N; act_id++) {
         auto& ref = elements[act_id];
+        size_t begin = table.size();
         if (!ref.empty()) {
+            std::map<union_type, std::vector<size_t>> valueToOffsetInTable;
             for (const auto& val_offset : ref) {
-                size_t val = storeLoad(val_offset.first);
                 for (const auto& traceid_eventid : val_offset.second) {
                     size_t offset =
                             trace_id_to_event_id_to_offset.at(traceid_eventid.first).at(traceid_eventid.second);
                     secondary_index[offset] = table.size();
-                    table.emplace_back(act_id, val, offset);
+                    valueToOffsetInTable[val_offset.first].emplace_back(offset);
                 }
+            }
+            for (auto it = valueToOffsetInTable.begin(); it != valueToOffsetInTable.end(); ) {
+                std::sort(it->second.begin(), it->second.end());
+                size_t val = storeLoad(it->first);
+                for (const auto& refx : it->second)
+                    table.emplace_back(act_id, val, refx);
+                it = valueToOffsetInTable.erase(it);
             }
             ref.clear();
         }
+        size_t end = table.size();
+        primary_index.emplace_back(begin, end);
     }
     elements.clear();
 }
@@ -117,6 +159,114 @@ std::ostream &AttributeTable::resolve_and_print(std::ostream &os, const Attribut
         default:
             // TODO: hierarchical types!, https://dl.acm.org/doi/10.1145/3410566.3410583
             return os << x.value;
+    }
+}
+
+#include <sstream>
+
+AttributeTable::range_query_result AttributeTable::range_query(const DataPredicate &prop, ssize_t act, double maximumApprox, const double c) const {
+    assert(prop.label == this->attr_name); // Just for consistency checking, I need to evaluate the predicate over the specific table. This is also for efficiency reasons
+    assert(maximumApprox >= 0.0); // Cannot have a negative approximation, as the approximation is just a distance
+    if (!prop.labelRHS.empty()) {
+        std::stringstream sstr;
+        sstr << "Predicate " << prop << ": cannot have a predicate over two distinct variables! ";
+        throw std::runtime_error(sstr.str());
+    }
+    if (!prop.exceptions.empty()) {
+        std::stringstream sstr;
+        sstr << "Predicate " << prop << ": cannot have excepted values at this stage: this should be an already decomposed interval ";
+        throw std::runtime_error(sstr.str());
+    }
+    if (!prop.BiVariableConditions.empty()) {
+        std::stringstream sstr;
+        sstr << "Predicate " << prop << ": cannot variable conditions that are proper to join conditions: either the decomposition is faulty, or the data clearing is not effective, or the interval decomposition isn't, or the interval is used inappropriately ";
+        throw std::runtime_error(sstr.str());
+    }
+    bool isNotExactMatch = maximumApprox > std::numeric_limits<double>::epsilon();
+    bool doWeHaveStrings = this->type == StringAtt;
+    if (doWeHaveStrings)
+        assert(ptr); // we need ptr for approximate string matching!
+    SimplifiedFuzzyStringMatching matchIfAttrIsString;
+
+    // At this stage, we know for sure that this element should be an interval at most.
+    // TTRUE -> RETURN everything
+    if (act == -1) {
+        AttributeTable::range_query_result result;
+        for (size_t actId = 0, N = primary_index.size(); actId < N; actId++) {
+            if (actId > primary_index.size()) return {}; // missing act
+            auto it = primary_index.at(actId);
+            if (it.first == it.second) return {}; // missing attribute ~ for the meantime, we are not approximating the match on the attribute name, but we should in the future
+            const AttributeTable::record * rbegin = &table[it.first];
+            const AttributeTable::record * rend = &table[it.second];
+            auto leftValue = resolve(table[it.first]);
+            auto rightValue = resolve(table[it.second]);
+            const union_type actualValue = cast_unions(type, prop.value);
+            switch (prop.casusu) {
+                case LT: {
+                    auto& thisResult = result.emplace_back();
+                    auto begin = table.begin() + it.first;
+                    auto end = table.begin() + it.second;
+                    auto rangeQuery = std::lower_bound(
+                            begin, end, actualValue,
+                            [&](const record &r, const union_type &value) {
+                                return resolve(r) < value;
+                            });
+                    bool hasSol = false;
+                    if (rangeQuery != end) {
+                        // something has been found!
+                        hasSol = true;
+                        thisResult.exact_solution.first =    (const AttributeTable::record*)&begin;
+                        thisResult.exact_solution.second =    (const AttributeTable::record*)&rangeQuery;
+
+                        // If we have a solution, then we still need to iterate for the remaining part, but only if
+                        // we have the need to an approximate solution
+                        if (isNotExactMatch) {
+                            if (doWeHaveStrings || (distanceFunction(resolve(*rangeQuery), actualValue)) < maximumApprox) {
+                                for (auto it2 = rangeQuery; it2 != end; it2++) {
+                                    if (doWeHaveStrings) {
+
+                                    } else {
+                                        double thisValue = distanceFunction(resolve(*it2), actualValue);
+                                        if (thisValue > maximumApprox)
+                                            break; // Stop the iteration if we reached the maximum part
+                                        thisResult.approx_solution.emplace((const AttributeTable::record*)&it2, thisValue);
+                                    }
+                                }
+                            }
+                        }
+
+                    } else {
+                        thisResult.exact_solution.first =    (const AttributeTable::record*)&end;
+                        thisResult.exact_solution.first =    (const AttributeTable::record*)&end;
+
+                        // If we have no solution, then we need to compute the distance for all of the elements
+                        // that are within this act.
+                        if (isNotExactMatch) {
+
+                        }
+                    }
+                }
+                    break;
+                case GT:
+                    break;
+                case LEQ:
+                    break;
+                case GEQ:
+                    break;
+                case EQ:
+                    break;
+                case NEQ:
+                    break;
+                case INTERVAL:
+                    break;
+                case TTRUE:
+                    // As the interval is true, return all of the elements that are in there
+                    result.emplace_back(rbegin, rend);
+                    break;
+            }
+        }
+    } else if (act >= 0) {
+
     }
 }
 
