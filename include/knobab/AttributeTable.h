a9be58a894e036e2420df1153bdc27fa663f7436
Giacomo Bergami <bergamigiacomo@gmail.com>
2022-01-17 20:05:46 +0000

    Supposedly finished to implement the data predicate

--- include/knobab/AttributeTable.h
+++ include/knobab/AttributeTable.h
@@ -30,18 +30,19 @@ using union_type = std::variant<double, size_t, long long, std::string, bool>;
 
 struct AttributeTable {
     std::string attr_name;
-    SimplifiedFuzzyStringMatching* ptr;
+    SimplifiedFuzzyStringMatching *ptr;
     AttributeTableType type;
-
+    std::unordered_map<std::string, std::vector<size_t>> string_offset_mapping;
 
     struct record {
-        act_t   act;
-        size_t  value;
-        size_t  act_table_offset;
+        act_t act;
+        size_t value;
+        size_t act_table_offset;
 
         record(act_t act, size_t value, size_t actTableOffset);
         DEFAULT_COPY_ASSGN(record);
     };
+
     std::vector<record> table;
 
 
@@ -49,14 +50,14 @@ struct AttributeTable {
         /**
          * Constructor to be used when an exact solution is always the case
          */
-        disjunctive_range_query_result(const record* begin, const record* end) : exact_solution(begin, end) {}
+        disjunctive_range_query_result(const record *begin, const record *end) : exact_solution(begin, end) {}
         DEFAULT_CONSTRUCTORS(disjunctive_range_query_result);
 
         /**
          * If the query is an approximated query, I cannot efficiently just provide a range of values where the values
          * are known for sure/certain that are going to be returned.
          */
-        std::unordered_map<const record*, double> approx_solution;
+        std::vector<std::pair<const record *, double>> approx_solution;
 
         /**
          * If the query requires the exact solution, the range of the records is return, without the need of copying the
@@ -64,8 +65,9 @@ struct AttributeTable {
          * is approximated, and there is still some data that we want to return, and therefore, some exact matches are
          * found
          */
-        std::pair<const record*, const record*> exact_solution;
+        std::pair<const record *, const record *> exact_solution;
     };
+
     using range_query_result = std::vector<disjunctive_range_query_result>;
 
     /**
@@ -81,17 +83,27 @@ struct AttributeTable {
     std::unordered_map<size_t, size_t> secondary_index;
 
     AttributeTable() : attr_name(""), ptr{nullptr}, type{BoolAtt} {}
-    AttributeTable(const std::string& attr, AttributeTableType type, SimplifiedFuzzyStringMatching* ptr = nullptr) : attr_name(attr), ptr{ptr}, type{type} {}
-    AttributeTable(const AttributeTable& ) = default;
-    AttributeTable(AttributeTable&& ) = default;
-    AttributeTable& operator=(const AttributeTable& ) = default;
-    AttributeTable& operator=(AttributeTable&& ) = default;
-    const record * resolve_record_if_exists(size_t actTableOffset) const;
-    std::ostream& resolve_and_print(std::ostream& os, const AttributeTable::record &x) const;
 
-    void record_load(act_t act_id, const union_type& val, trace_t tid, event_t eid);
+    AttributeTable(const std::string &attr, AttributeTableType type, SimplifiedFuzzyStringMatching *ptr = nullptr)
+            : attr_name(attr), ptr{ptr}, type{type} {}
+
+    AttributeTable(const AttributeTable &) = default;
+
+    AttributeTable(AttributeTable &&) = default;
+
+    AttributeTable &operator=(const AttributeTable &) = default;
+
+    AttributeTable &operator=(AttributeTable &&) = default;
+
+    const record *resolve_record_if_exists(size_t actTableOffset) const;
+
+    std::ostream &resolve_and_print(std::ostream &os, const AttributeTable::record &x) const;
+
+    void record_load(act_t act_id, const union_type &val, trace_t tid, event_t eid);
+
     void index(const std::vector<std::vector<size_t>> &trace_id_to_event_id_to_offset);
-    union_type resolve(const record& x) const;
+
+    union_type resolve(const record &x) const;
 
     /**
      *
@@ -103,15 +115,38 @@ struct AttributeTable {
      *                          less distance than the given one
      * @return
      */
-    range_query_result range_query(const DataPredicate &prop, ssize_t act = -1, double maximumApprox = 0.0, const double c = 2.0) const;
+    range_query_result
+    range_query(DataPredicate prop, ssize_t act = -1, double maximumApprox = 0.0, const double c = 2.0) const;
 
 private:
     std::vector<std::map<union_type, std::vector<std::pair<trace_t, event_t>>>> elements;
 
     size_t storeLoad(const union_type &x);
-    bool assertVariant(const union_type& val);
+
+    bool assertVariant(const union_type &val);
+
+    bool range_query(size_t actId, const DataPredicate &prop, AttributeTable::range_query_result &result,
+                     bool isNotExactMatch, double min_threshold) const ;
+
+    template <typename T>
+    void
+    getIterator(double min_threshold, AttributeTable::disjunctive_range_query_result &thisResult,
+                SimplifiedFuzzyStringMatching &sfzm,
+                const T& i) const {
+        std::multimap<double, std::string> result;
+        sfzm.fuzzyMatch(min_threshold, 1, get<std::string>(resolve(*i)), result); // IDE has a problem, but this is going to be expanded in the Cpp!
+        if (!result.empty()) {
+            for (const auto& ref : result) {
+                auto it2 = string_offset_mapping.find(ref.second);
+                if (it2 != string_offset_mapping.end()) {
+                    for (const size_t offset : it2->second) {
+                        thisResult.approx_solution.emplace_back((const record*)(&this->table[offset]), ref.first);
+                    }
+                }
+            }
+        }
+    }
+
 };
-
-
 
 #endif //BZDB_ATTRIBUTETABLE_H
