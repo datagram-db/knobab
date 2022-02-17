2ec711851e62dd3210bc7b6d662f9ce4c3f063e8
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-02-17 18:53:36 +0000

    Range Queries were defined as views in a proper way

---
+++ DataRepresentation.cpp
@@ -0,0 +1,63 @@
+//
+// Created by giacomo on 17/02/2022.
+//
+
+#include "DataRepresentation.h"
+
+DataRepresentationEvent::DataRepresentationEvent(uint32_t traceId, uint16_t eventId, double satisfiability,
+                                                 const std::vector<size_t> &activationOrTarget) : trace_id(traceId),
+                                                                                                    event_id(eventId),
+                                                                                                    satisfiability(
+                                                                                                            satisfiability),
+                                                                                                    activation_or_target(
+                                                                                                            activationOrTarget) {}
+
+bool DataRepresentationEvent::operator==(const DataRepresentationEvent &rhs) const {
+    return trace_id == rhs.trace_id &&
+           event_id == rhs.event_id &&
+           satisfiability == rhs.satisfiability &&
+           activation_or_target == rhs.activation_or_target;
+}
+
+bool DataRepresentationEvent::operator!=(const DataRepresentationEvent &rhs) const {
+    return !(rhs == *this);
+}
+
+bool DataRepresentationEvent::operator<(const DataRepresentationEvent &rhs) const {
+    if (trace_id < rhs.trace_id)
+        return true;
+    if (rhs.trace_id < trace_id)
+        return false;
+    if (event_id < rhs.event_id)
+        return true;
+    if (rhs.event_id < event_id)
+        return false;
+    if (satisfiability < rhs.satisfiability)
+        return true;
+    if (rhs.satisfiability < satisfiability)
+        return false;
+    return activation_or_target < rhs.activation_or_target;
+}
+
+bool DataRepresentationEvent::operator>(const DataRepresentationEvent &rhs) const {
+    return rhs < *this;
+}
+
+bool DataRepresentationEvent::operator<=(const DataRepresentationEvent &rhs) const {
+    return !(rhs < *this);
+}
+
+bool DataRepresentationEvent::operator>=(const DataRepresentationEvent &rhs) const {
+    return !(*this < rhs);
+}
+
+std::ostream& operator<<(std::ostream& os, const DataRepresentationEvent &event) {
+    os << "trace_id: " << event.trace_id << " event_id: " << event.event_id << " satisfiability: "
+       << event.satisfiability << " activation_or_target: {";
+    for (size_t i = 0, N = event.activation_or_target.size(); i<N; i++) {
+        os << event.activation_or_target.at(i);
+        if (i != (N-1))
+            os << ", ";
+    }
+    return os <<"}";
+}
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include "RangeQueryIterator.h"
+
+
+int main() {
+
+    std::pair<int, std::vector<std::pair<std::pair<uint32_t, uint16_t>, double>>> cp;
+    cp.first = 1;
+    std::vector<size_t> W{1,2,3};
+
+    {
+        RangeQueryIterator beg(&cp, true, 3, W, true);
+        RangeQueryIterator end(&cp, false, 3, W, true);
+
+        while (beg != end) {
+            std::cout << *beg << std::endl;
+            beg++;
+        }
+    }
+
+    std::cout << "-----------------------------------------------------------" << std::endl;
+
+    cp.first = 2;
+    cp.second.emplace_back(std::pair<uint32_t, uint16_t>{0,0},1);
+    cp.second.emplace_back(std::pair<uint32_t, uint16_t>{1,1},1);
+    cp.second.emplace_back(std::pair<uint32_t, uint16_t>{1,2},1);
+    cp.second.emplace_back(std::pair<uint32_t, uint16_t>{3,1},1);
+    cp.second.emplace_back(std::pair<uint32_t, uint16_t>{4,2},1);
+    W.emplace_back(5);
+
+    {
+        RangeQueryIterator beg(&cp, true, 3, W, true);
+        RangeQueryIterator end(&cp, false, 3, W, true);
+
+        while (beg != end) {
+            std::cout << *beg << std::endl;
+            beg++;
+        }
+    }
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
