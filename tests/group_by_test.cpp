094ad0fb0e97155e060c503f92f04c6e852dc4fa
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-02-27 22:01:24 +0000

    gtest

--- tests/group_by_test.cpp
+++ tests/group_by_test.cpp
@@ -9,27 +9,27 @@
 #include <yaucl/hashing/pair_hash.h>
 #include <yaucl/hashing/uset_hash.h>
 #include <yaucl/structures/set_operations.h>
+#include <gtest/gtest.h>
 
-#include "../submodules/ctest/ctest.h"
+class groupby : public ::testing::Test {
+protected:
+    void SetUp() override {
+        W = {{1,2}, {3,0.5}, {3, 0.7}, {2, 0.5}, {1, 7}};
+        std::sort(W.begin(), W.end());
+    }
 
-CTEST_DATA(groupby) {
     std::vector <std::pair<size_t, double>> W;
 };
 
-CTEST_SETUP(groupby) {
-    data->W = {{1,2}, {3,0.5}, {3, 0.7}, {2, 0.5}, {1, 7}};
-    std::sort(data->W.begin(), data->W.end());
+TEST_F(groupby, groupby) {
+    auto S = GroupByKeyExtractorIgnoreKey<std::vector<std::pair<size_t, double>>::iterator, size_t, std::pair<size_t, double>>(W.begin(), W.end(), [](const std::pair<size_t, double>& x) {return x.first; });
+    EXPECT_EQ(S, (std::vector<std::vector<std::pair<size_t, double>>>{{{1,2},{1,7}}, {{2,0.5}}, {{3,0.5}, {3,0.7}}}));
 }
 
-CTEST2(groupby, groupby) {
-    auto S = GroupByKeyExtractorIgnoreKey<std::vector<std::pair<size_t, double>>::iterator, size_t, std::pair<size_t, double>>(data->W.begin(), data->W.end(), [](const std::pair<size_t, double>& x) {return x.first; });
-    ASSERT_TRUE(S == (std::vector<std::vector<std::pair<size_t, double>>>{{{1,2},{1,7}}, {{2,0.5}}, {{3,0.5}, {3,0.7}}}));
-}
-
-CTEST2(groupby, cartesian_product) {
-    auto S = GroupByKeyExtractorIgnoreKey<std::vector<std::pair<size_t, double>>::iterator, size_t, std::pair<size_t, double>>(data->W.begin(), data->W.end(), [](const std::pair<size_t, double>& x) {return x.first; });
+TEST_F(groupby, cartesian_product) {
+    auto S = GroupByKeyExtractorIgnoreKey<std::vector<std::pair<size_t, double>>::iterator, size_t, std::pair<size_t, double>>(W.begin(), W.end(), [](const std::pair<size_t, double>& x) {return x.first; });
     auto M = cartesian_product(S);
-    ASSERT_TRUE(M ==
+    EXPECT_EQ(M,
            (std::unordered_set<std::unordered_set<std::pair<size_t, double>>>{{{1,7},{2,0.5},{3,0.7}},
                                                                               {{1,7},{2,0.5},{3,0.5}},
                                                                               {{1,2},{2,0.5}, {3,0.7}},
---
+++ tests/single_declare_correctness.cpp
@@ -0,0 +1,15 @@
+//
+// Created by giacomo on 27/02/2022.
+//
+
+#define CTEST_COLOR_OK
+#define CTEST_SEGFAULT
+
+#include <vector>
+#include <yaucl/structures/set_operations.h>
+#include <knobab/Environment.h>
+#include <gtest/gtest.h>
+
+TEST(single_declare, init) {
+
+}
