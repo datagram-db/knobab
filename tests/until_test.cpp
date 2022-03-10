7e2a6cb3081fe0731d81928eb5810575b721f3ba
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-03-10 14:06:34 +0000

    logic_until_untimed, tested

--- tests/until_test.cpp
+++ tests/until_test.cpp
@@ -10,17 +10,20 @@
 #include <knobab/Environment.h>
 
 #include <gtest/gtest.h>
+#include <knobab/operators/simple_ltlf_operators.h>
 #include "log_for_tests.h"
 
 class until_tests : public testing::Test {
 protected:
     void SetUp() override {
-        ss << logUntil;
-        env.load_log(HUMAN_READABLE_YAUCL, true, "logUntil.txt", false, ss);
+        auto file = std::filesystem::current_path().parent_path().parent_path() / "data" / "testing" / "logUntil.txt";
+        {
+            std::ifstream if_{file};
+            env.load_log(HUMAN_READABLE_YAUCL, true, file.string(), false, if_);
+        }
     }
 
     Environment env;
-    std::stringstream ss;
 };
 
 
@@ -29,6 +32,16 @@ TEST_F(until_tests, basic) {
     auto b = env.db.exists("B", true);
     std::set<uint32_t> expectedTraces{1,3,5,7,9,10,11,12,13};
     auto result = until(a, b, env.db.act_table_by_act_id.getTraceLengths(), nullptr);
+    for (const auto& ref : result)
+        EXPECT_TRUE(expectedTraces.contains(ref.first.first));
+}
+
+TEST_F(until_tests, logic) {
+    auto a = env.db.exists("A", true);
+    auto b = env.db.exists("B", true);
+    std::set<uint32_t> expectedTraces{1,3,5,7,9,10,11,12,13};
+    dataContainer result;
+    until_logic_untimed(a, b, env.db.act_table_by_act_id.getTraceLengths(), result, nullptr);
     for (const auto& ref : result)
         EXPECT_TRUE(expectedTraces.contains(ref.first.first));
 }
@@ -43,3 +56,13 @@ TEST_F(until_tests, predicate_manager) {
         EXPECT_TRUE(expectedTraces.contains(ref.first.first));
 }
 
+TEST_F(until_tests, logic_pm) {
+    auto a = env.db.exists("A", true);
+    auto b = env.db.exists("B", true);
+    PredicateManager pm{{{{"x", "y", LT}}}, &env.db};
+    std::set<uint32_t> expectedTraces{1,3,5,7,13};
+    dataContainer result;
+    until_logic_untimed(a, b, env.db.act_table_by_act_id.getTraceLengths(), result, &pm);
+    for (const auto& ref : result)
+        EXPECT_TRUE(expectedTraces.contains(ref.first.first));
+}
