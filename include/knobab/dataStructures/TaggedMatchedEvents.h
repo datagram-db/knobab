bc0331550211d96bd84586153ce77dc00a96325d
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-03-11 11:57:38 +0000

    added a better data structure to get activation and target events

---
+++ include/knobab/dataStructures/TaggedMatchedEvents.h
@@ -0,0 +1,54 @@
+//
+// Created by giacomo on 11/03/2022.
+//
+
+#ifndef KNOBAB_TAGGEDMATCHEDEVENTS_H
+#define KNOBAB_TAGGEDMATCHEDEVENTS_H
+
+#include <knobab/dataStructures/oid.h>
+
+#define MARKED_EVENT_LEFT           (1)
+#define MARKED_EVENT_ACTIVATION     MARKED_EVENT_LEFT
+#define MARKED_EVENT_RIGHT          (2)
+#define MARKED_EVENT_TARGET         MARKED_EVENT_RIGHT
+#define MARKED_EVENT_MATCH          (3)
+
+using marked_event_type = uint16_t;
+using marked_event_future_t = uint16_t;
+
+TAGGED_UNION_WITH_ENCAPSULATION_BEGIN(uint64_t, marked_event, 0, 4, marked_event_type type:16, event_t left:16, event_t right:16, marked_event_future_t future:16)
+    (marked_event_type type, event_t left, event_t right = 0, marked_event_future_t future = 0) {
+        id.parts.type = type;
+        id.parts.left = left;
+        id.parts.right = right;
+        id.parts.future = future;
+    }
+
+    static marked_event left(event_t left) {
+        return {MARKED_EVENT_ACTIVATION, left};
+    }
+    static marked_event activation(event_t left) {
+        return {MARKED_EVENT_ACTIVATION, left};
+    }
+    static marked_event right(event_t right) {
+        return {MARKED_EVENT_ACTIVATION, 0, right};
+    }
+    static marked_event target(event_t right) {
+        return {MARKED_EVENT_ACTIVATION, 0, right};
+    }
+    static marked_event join(event_t left, event_t right) {
+        return {MARKED_EVENT_MATCH, left, right};
+    }
+
+TAGGED_UNION_ENCAPSULATOR_END
+
+#define IS_MARKED_EVENT_LEFT(x)  (((marked_event)(x)).id.parts.type == MARKED_EVENT_LEFT)
+#define IS_MARKED_EVENT_ACTIVATION(x)  (((marked_event)(x)).id.parts.type == MARKED_EVENT_ACTIVATION)
+#define IS_MARKED_EVENT_RIGHT(x)  (((marked_event)(x)).id.parts.type == MARKED_EVENT_RIGHT)
+#define IS_MARKED_EVENT_TARGET(x)  (((marked_event)(x)).id.parts.type == MARKED_EVENT_TARGET)
+#define GET_ACTIVATION_EVENT(x)    (IS_MARKED_EVENT_ACTIVATION(x) ? (x).id.parts.left : 0)
+#define GET_TARGET_EVENT(x)    (IS_MARKED_EVENT_TARGET(x) ? (x).id.parts.left : 0)
+#define GET_LEFT_EVENT(x)    (IS_MARKED_EVENT_LEFT(x) ? (x).id.parts.left : 0)
+#define GET_RIGHT_EVENT(x)    (IS_MARKED_EVENT_RIGHT(x) ? (x).id.parts.left : 0)
+
+#endif //KNOBAB_TAGGEDMATCHEDEVENTS_H
