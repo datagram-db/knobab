14d36a00c7e458fbee75ca8c2d03c5c07f15fb43
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-03-11 11:36:15 +0000

    Tagged union, now defined as macro. The rational is the following: whenever I want to use packed bits to do that, I shall not re-create the same stuff all over again!

--- src/knobab/oid.cpp
+++ src/knobab/oid.cpp
@@ -4,31 +4,23 @@
 
 #include <knobab/dataStructures/oid.h>
 
+
+#if 0
 oid::oid() : oid(0) {}
-
 oid::oid(const monotone_hash_t &x) {
-    id.monotone_hash = x;
+    id.data = x;
 }
-
 oid::oid(act_t fact, trace_t trace_id, event_t event_pos) {
-    //id.parts.future = 0;
     id.parts.act = fact;
     id.parts.trace_id = trace_id;
     id.parts.event_id = event_pos;
 }
-
-oid &oid::operator=(const oid &x) { id.monotone_hash = x.id.monotone_hash; return *this; }
-
-oid::oid(const oid &x) { id.monotone_hash = x.id.monotone_hash; }
-
-bool oid::operator<(const oid &rhs) const { return id.monotone_hash < rhs.id.monotone_hash;  }
-
+oid &oid::operator=(const oid &x) { id.data = x.id.data; return *this; }
+oid::oid(const oid &x) { id.data = x.id.data; }
+bool oid::operator<(const oid &rhs) const { return id.data < rhs.id.data;  }
 bool oid::operator>(const oid &rhs) const { return rhs < *this;     }
-
 bool oid::operator<=(const oid &rhs) const { return !(rhs < *this);  }
-
 bool oid::operator>=(const oid &rhs) const { return !(*this < rhs);  }
-
-bool oid::operator==(const oid &rhs) const { return id.monotone_hash == rhs.id.monotone_hash; }
-
+bool oid::operator==(const oid &rhs) const { return id.data == rhs.id.data; }
 bool oid::operator!=(const oid &rhs) const { return !(rhs == *this); }
+#endif
