eec0acd2e9a97ca14e7ae266e9fd41a954ec089e
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-03-15 13:01:26 +0000

    Moved python scripts to a specific folder

---
+++ scripts/generate_ltlf_patterns.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import flloat.parser.ltlf
+import flloat.ltlf
+import os
+
+def serialize(ltlf, file):
+	print(str(flloat.parser.ltlf.LTLfParser()(ltlf).to_automaton().to_graphviz()), file=open(file, 'w'))
+
+serialize("a U b", os.path.join(os.getcwd(), os.pardir, 'data', 'cache', 'graph_pattern_cache', 'Until'))
+serialize("a R b", os.path.join(os.getcwd(), os.pardir, 'data', 'cache', 'graph_pattern_cache', 'Release'))
+serialize("F a", os.path.join(os.getcwd(), os.pardir, 'data', 'cache', 'graph_pattern_cache', 'Diamond'))
+serialize("G a", os.path.join(os.getcwd(), os.pardir, 'data', 'cache', 'graph_pattern_cache', 'Box'))
+serialize("(a U b) | G(b)", os.path.join(os.getcwd(), os.pardir, 'data', 'cache', 'graph_pattern_cache', 'WeakUntil'))
