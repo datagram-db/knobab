ba0dae695b2a4919e580ab6931c2034d9e6fd2ca
Giacomo Bergami <ngb113@newcastle.ac.uk>
2022-04-14 20:51:37 +0100

    Setted the scripts for running the pipeline in parallel! it assumes that ./fast_build/ is the location where the Release folder for the compile is located

---
+++ parallelization_run.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+# Declare a string array with type
+declare -a StringArray=("AltPrecedence" "AltResponse" "ChainPrecedence" "ChainResponse" "NegSuccession" "Precedence" "RespExistence" "Response")
+declare -a ProcArray=("scripts/maxsat_pipeline2.yaml" "scripts/maxsat_pipeline4.yaml" "scripts/maxsat_pipeline6.yaml" "scripts/maxsat_pipeline8.yaml")
+declare -a plist=(2 4 8 16)
+ 
+ 
+# Read the array values with space
+for ms in "${plist[@]}"; do
+	for val in "${StringArray[@]}"; do
+		./fast_build/server -n -o -q 5 -p $ms --topNTemplate=$val --xes=/home/giacomo/projects/knobab/data/testing/hospital/standard/Hospital_Log.xes  --maxsat=scripts/maxsat_pipeline.yaml --csv=parall.csv
+		for proc in "${ProcArray[@]}"; do
+			echo "$ms $val $proc"
+			sleep 3
+			./parall/server -n -o -q 5 -p $ms --topNTemplate=$val --xes=/home/giacomo/projects/knobab/data/testing/hospital/standard/Hospital_Log.xes  --maxsat=$proc --csv=parall.csv
+		done
+	done
+done
+
+
+
