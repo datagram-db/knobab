89bd1f660200ac731be556729941f446e83abee3
SamuelAppleby <sambuzzappleby@hotmail.co.uk>
2022-04-25 17:19:00 +0100

    Latest R scripts and data changes

--- competitors/SQLMinerBenchmarker/graphing/sql_benchmarker.R
+++ competitors/SQLMinerBenchmarker/graphing/sql_benchmarker.R
@@ -4,32 +4,32 @@ library(reshape)
 
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-timings <- read.csv(file = 'C:/Users/Sam/Documents/Repositories/Codebases/knobab/data/testing/results/knobab_sql_benchmark.csv')
-timings = timings[,c('n_traces', 'model_filename', 'atomization_conf', 'model_ltlf_query_time')]
+knoababtimings <- read.csv(file = 'C:/Users/Sam/Documents/Repositories/Codebases/knobab/data/testing/results/knobab_sql_benchmark.csv')
 
-timings <- aggregate(list(model_ltlf_query_time = timings$model_ltlf_query_time),
-                     by=list(atomization_conf = timings$atomization_conf, n_traces=timings$n_traces,model_filename=timings$model_filename),data=timings,FUN=mean)
+knoababtimings$execution_time <- knoababtimings$model_data_decomposition_time + knoababtimings$model_atomization_time + knoababtimings$model_declare_to_ltlf + knoababtimings$model_ltlf_query_time
 
-knobabdf <- melt(timings, id.vars = c("n_traces", "model_filename", "atomization_conf","model_ltlf_query_time"))
+knoababtimings = knoababtimings[,c('n_traces', 'model_filename', 'atomization_conf', 'execution_time')]
+
+knoababtimings <- aggregate(list(execution_time = knoababtimings$execution_time),
+                     by=list(atomization_conf = knoababtimings$atomization_conf, n_traces=knoababtimings$n_traces,model_filename=knoababtimings$model_filename),data=knoababtimings,FUN=mean)
+
+knobabdf <- as.data.frame(melt(knoababtimings, id.vars = c("n_traces", "model_filename", "atomization_conf","execution_time")))
 
 sqltimings <- read.csv(file = 'C:/Users/Sam/Documents/Repositories/Codebases/knobab/data/testing/results/sql_benchmark.csv')
 
-sqltimings <- aggregate(list(sql_execution_time = sqltimings$sql_execution_time),
-                        by=list(n_traces=sqltimings$n_traces,model_filename=sqltimings$model_filename,query_type=sqltimings$query_type,failure=sqltimings$failure),data=sqltimings,FUN=mean)
+sqltimings <- aggregate(list(execution_time = sqltimings$execution_time),
+                        by=list(n_traces=sqltimings$n_traces,model_filename=sqltimings$model_filename,atomization_conf=sqltimings$atomization_conf,failure=sqltimings$failure),data=sqltimings,FUN=mean)
 
-validsqltimings <- subset(sqltimings, failure=="FALSE")
+validsqltimings <- subset(sqltimings, select = c(atomization_conf,n_traces,model_filename,execution_time), failure=="FALSE")
 
-sqlsuccessdf <- melt(validsqltimings, id.vars = c("n_traces", "model_filename", "query_type","sql_execution_time", "failure"))
+validsqltimings$failure <- NULL
 
-invalidsqltimings <- subset(sqltimings, failure=="TRUE")
+sqlsuccessdf <- as.data.frame(melt(validsqltimings, id.vars = c("n_traces", "model_filename", "atomization_conf","execution_time")))
 
-sqlfaileddf <- melt(invalidsqltimings, id.vars = c("n_traces", "model_filename", "query_type","sql_execution_time", "failure"))
-
-ggplot(sqlsuccessdf, aes(x=n_traces, y=sql_execution_time), group=query_type) +
-  geom_line(aes(color=query_type)) +
-  geom_point(aes(color=query_type)) +
-  geom_line(data = knobabdf, aes(x=n_traces, y = model_ltlf_query_time, color=atomization_conf)) +
-  geom_point(data = knobabdf, aes(x = n_traces, y = model_ltlf_query_time, color=atomization_conf)) +
+rbind(knobabdf, sqlsuccessdf) %>%
+ggplot(aes(x=n_traces, y=execution_time), group=atomization_conf) +
+  geom_line(aes(color=atomization_conf)) +
+  geom_point(aes(color=atomization_conf)) +
   facet_wrap( ~ model_filename, nrow=2, ncol=4) +
   labs(x = "Log Size", y = "Time (ms)") + 
   scale_y_continuous(trans='log10') + 
