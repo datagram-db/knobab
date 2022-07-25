febd4637d88ddab613cc5d1f8a2273272844e392
Samuel Appleby <sambuzzappleby@hotmail.co.uk>
2022-07-25 15:05:22 +0100

    Readded ideas2022 data. New benchmarks complete. New data added.

--- data/testing/r_scripts/burattin_benchmarker.R
+++ data/testing/r_scripts/burattin_benchmarker.R
@@ -5,7 +5,7 @@ library(dplyr)
 library(tidyr)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
-knoababtimings <- read.csv(file = 'C:/Users/Sam/Documents/Repositories/Codebases/knobab/data/testing/results/knobab_burattin_benchmark.csv')
+knoababtimings <- read.csv(file = '/media/sam/0E68C35868C33CE9/Users/sambu/Documents/Repositories/CodeBases/knobab/data/testing/results/knobab_burattin_benchmark.csv')
 
 knoababtimings$execution_time <- knoababtimings$model_data_decomposition_time + knoababtimings$model_atomization_time + knoababtimings$model_declare_to_ltlf + knoababtimings$model_ltlf_query_time
 
@@ -16,20 +16,24 @@ knoababtimings <- aggregate(list(execution_time = knoababtimings$execution_time)
 
 knobabdf <- as.data.frame(melt(knoababtimings, id.vars = c("model_filename", "atomization_conf","execution_time")))
 
-burattintimings <- read.csv(file = 'C:/Users/Sam/Documents/Repositories/Codebases/knobab/data/testing/results/burattin_benchmark.csv')
+burattintimings <- read.csv(file = '/media/sam/0E68C35868C33CE9/Users/sambu/Documents/Repositories/CodeBases/knobab/data/testing/results/burattin_benchmark.csv')
 
 burattintimings <- aggregate(list(execution_time = burattintimings$execution_time),
                         by=list(model_filename=burattintimings$model_filename,atomization_conf=burattintimings$atomization_conf),data=burattintimings,FUN=mean)
 
 burattindf <- as.data.frame(melt(burattintimings, id.vars = c("model_filename", "atomization_conf", "execution_time")))
 
-rbind(knobabdf, burattindf)  %>%
-ggplot( aes(x=model_filename, y=execution_time, fill=atomization_conf)) +
+combineddf <- rbind(knobabdf, burattindf)  
+
+combineddf <- combineddf[order(combineddf[,1], combineddf[,3]), ]
+
+combineddf$atomization_conf <- factor(combineddf$atomization_conf, levels=c("KnoBAB + CQ", "KnoBAB + Support", "Declare Analyzer"))
+
+ggplot(combineddf, aes(x=model_filename, y=execution_time, fill=atomization_conf)) +
   geom_bar(position = "dodge", stat = "identity") +
   labs(x = "Model", y = "Time (ms)") + 
   theme(legend.position="bottom") + 
   guides(fill=guide_legend(title="")) +
   scale_y_log10(
     breaks = scales::trans_breaks("log10", function(x) 10^x),
-    labels = scales::trans_format("log10", scales::math_format(10^.x))
-)
+    labels = scales::trans_format("log10", scales::math_format(10^.x)))
