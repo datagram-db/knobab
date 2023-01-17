e4ffdf5594e04475b6bd8e13ca42ff40b522b226
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-01-17 19:47:30 +0000

    Data loading upgrade, fix

--- src/knobab/mining/mining.cpp
+++ src/knobab/mining/mining.cpp
@@ -8,10 +8,8 @@
 int main() {
     Environment env;
     env.doStats = false;
-    env.set_grounding_parameters(true, false, true,GroundingStrategyConf::NO_EXPANSION);
-    env.set_atomization_parameters("p", 20);
     auto scripts = std::filesystem::current_path();
-    std::filesystem::path file{"/home/giacomo/projects/knobab/data/benchmarking/1000_10000_log.tab"};
+    std::filesystem::path file{"/home/giacomo/projects/knobab2/data/benchmarking/completed/1000_1000_log.tab"};
 //    auto file = scripts / "data" / "testing" / "declare" / "mining" / "response_test";
     {
         std::ifstream if_{file};
@@ -20,8 +18,8 @@ int main() {
     std::filesystem::path declare_file_path, maxsat;
     std::cout << "Starting from now!" << std::endl;
     auto list = pattern_mining(env.db, 0.01, false, true, true, false, false);
-//    for (const auto& result : list) {
-//        std::cout << result << std::endl;
-//    }
+    for (const auto& result : list) {
+        std::cout << result << std::endl;
+    }
     return 0;
 }
