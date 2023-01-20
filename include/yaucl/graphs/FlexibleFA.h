419f476af6fd1ffe663da5e31aa18a68e1645f33
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-01-20 00:17:00 +0000

    A mildly good trace generator, in tab format

--- include/yaucl/graphs/FlexibleFA.h
+++ include/yaucl/graphs/FlexibleFA.h
@@ -35,10 +35,74 @@
 #include <variant>
 #include <cassert>
 #include <yaucl/graphs/NodeLabelBijectionFA.h>
+#include <random>
 #include <iostream>
 #include <sstream>
 #include <fstream>
 #include <queue>
+
+// C++ program for the above approach
+
+#include <bits/stdc++.h>
+
+template <typename T>
+struct QNode {
+    T data;
+    QNode<T>* next;
+    QNode(const T& d)
+    {
+        data = d;
+        next = NULL;
+        }
+};
+template <typename T>
+struct Queue {
+    QNode<T> *front, *rear;
+    Queue() { front = rear = NULL; }
+    
+    void enQueue(T x)
+    {
+        
+        // Create a new LL node
+        QNode<T>* temp = new QNode(x);
+        
+        // If queue is empty, then
+        // new node is front and rear both
+        if (rear == NULL) {
+            front = rear = temp;
+            return;
+            }
+        
+        // Add the new node at
+        // the end of queue and change rear
+        rear->next = temp;
+        rear = temp;
+        }
+
+        bool empty() const { return front == NULL; }
+        const T& top() const { return front->data; }
+    
+    // Function to remove
+    // a key from given queue q
+    void pop()
+    {
+        // If queue is empty, return NULL.
+        if (front == NULL)
+        return;
+        
+        // Store previous front and
+        // move front one node ahead
+        QNode<T>* temp = front;
+        front = front->next;
+        
+        // If front becomes NULL, then
+        // change rear also as NULL
+        if (front == NULL)
+        rear = NULL;
+        
+        delete (temp);
+        }
+};
 
 template <typename NodeElement, typename EdgeLabel>
 class FlexibleFA : public FlexibleGraph<NodeElement, EdgeLabel> {
@@ -62,28 +126,51 @@ public:
         return final_nodes;
     }
 
-    std::set<std::vector<EdgeLabel>> generative(size_t limitSize = 3, size_t max_per_limit = 50) {
-        std::vector<size_t> Arg(getNodeIds().size(), 0);
-        std::queue<std::pair<size_t, std::vector<EdgeLabel>>> Q;
-        std::set<std::vector<EdgeLabel>> EL;
+//    std::set<std::vector<EdgeLabel>>
+    void generative(std::ofstream& tab,
+                                                size_t min_len = 3,
+                                                size_t max_len = 3,
+                                                size_t max_per_limit = std::numeric_limits<size_t>::max(),
+                                                bool doSample = false,
+                                                double sampleRate = 0.5) {
+        if (max_per_limit == 0) return;
+        std::default_random_engine generator;
+        std::uniform_real_distribution<double> distribution(0.0,1.0);
+//        std::vector<size_t> Arg(getNodeIds().size(), 0);
+        Queue<std::pair<size_t, std::vector<EdgeLabel>>> Q;
+        std::pair<size_t, std::vector<EdgeLabel>> cp;
+//        std::set<std::vector<EdgeLabel>> EL;
         for (size_t i : init())
-            Q.emplace(i, std::vector<EdgeLabel>{});
+            Q.enQueue(std::make_pair(i, std::vector<EdgeLabel>{}));
         while (!Q.empty()) {
-            std::pair<size_t, std::vector<EdgeLabel>> cp = Q.front();
+            std::swap(cp, Q.front->data);
             Q.pop();
-            if ((cp.second.size() <= limitSize)) {
+            if ((cp.second.size() <= max_len)) {
                 if (final_nodes.contains(cp.first) && (cp.second.size() > 0) /*&& (Arg[cp.first] < max_per_limit)*/) {
-                    //Arg[cp.first]++;
-                    EL.emplace(cp.second);
+//                    Arg[cp.first]++;
+//                    EL.emplace(cp.second);
+                    if (min_len <= cp.second.size()) {
+                        for (size_t j = 0, N = cp.second.size()-1; j<=N; j++) {
+                            tab << cp.second[j];
+                            if (j != N) tab  << "\t";
+                        }
+                        tab << std::endl;
+                        std::flush(tab);
+                        max_per_limit--;
+                        if (max_per_limit == 0) return;
+                    }
                 }
                 for (auto cp2 : outgoingEdges(cp.first)) {
-                    std::vector<EdgeLabel> el{cp.second};
-                    el.emplace_back(cp2.first);
-                    Q.emplace(cp2.second, el);
+                    double number = distribution(generator);
+                    if ((!doSample) || number <= sampleRate) {
+                        std::vector<EdgeLabel> el{cp.second};
+                        el.emplace_back(cp2.first);
+                        Q.enQueue(std::make_pair(cp2.second, el));
+                    }
                 }
             }
         }
-        return EL;
+//        return EL;
     }
 
     std::vector<size_t> getNodeIds() const  {
