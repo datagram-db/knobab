3cd6bfd11101436c91092932b3fb1faebb832aee
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-01-25 23:40:27 +0000

    Better script

--- query_compiler/compile_to_languages.sh
+++ query_compiler/compile_to_languages.sh
@@ -6,10 +6,14 @@ mkdir python3
 pushd src
 haxe --java ../java/ kinj/KINJ.hx
 haxe --cs   ../cs/   kinj/KINJ.hx
-#haxe --cpp  ../cpp   kinj/KINJ.jx
 haxe --python  ../python3/kinj.py   kinj/KINJ.hx
 popd 
 rm -rf kinj
 mvn archetype:generate -DgroupId=uk.knobab -DartifactId=kinj -DarchetypeArtifactId=maven-archetype-quickstart -DarchetypeVersion=1.4 -DinteractiveMode=false
 cp -r java/src kinj/src/main/java
+pushd kinj
+mvn test-compile
+popd
 rm -rf java
+rm -rf cs/bin/*
+echo '#!/usr/bin/env python3' | cat - python3/kinj.py > temp && mv temp python3/kinj.py
