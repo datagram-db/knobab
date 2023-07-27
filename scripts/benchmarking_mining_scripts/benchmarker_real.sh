961fcd757bdc7a8d12f7279671b3d91e44e8f51b
Giacomo Bergami <bergamigiacomo@gmail.com>
2023-07-27 20:07:50 +0100

    Version used for benchmarking

--- scripts/benchmarking_mining_scripts/benchmarker_real.sh
+++ scripts/benchmarking_mining_scripts/benchmarker_real.sh
@@ -1,6 +1,6 @@
 set -x #echo on
 support=(0.1 0.25 0.5 0.9)
-algorithm=("apriori" "topk" "bolt2")
+algorithm=("bolt2" "apriori")
 logs=("data/benchmarking/mining/bpic_2019/logs/10.xes" "data/benchmarking/mining/bpic_2019/logs/100.xes" "data/benchmarking/mining/bpic_2019/logs/1000.xes" "data/benchmarking/mining/bpic_2019/logs/10000.xes" "data/benchmarking/mining/bpic_2019/logs/100000.xes")
 
 for s in "${support[@]}"; do
