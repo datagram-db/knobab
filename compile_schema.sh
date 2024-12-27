36ae8e441a8928756bdaa3ce828e83fc04419137
Giacomo Bergami <bergamigiacomo@gmail.com>
2024-12-27 19:19:26 +0000

    Keeping just the minimal element of interest

--- compile_schema.sh
+++
@@ -1,2 +0,0 @@
-export CAPNP="/home/giacomo/projects/DTMining/cmake-build-debug/submodules/capnproto/c++/src/capnp/" #"/home/giacomo/projects/tiny_datagramdb/submodules/capnproto/build/c++/src/capnp/capnp"
-$CAPNP/capnp compile -o$CAPNP/capnpc-c++ $1
